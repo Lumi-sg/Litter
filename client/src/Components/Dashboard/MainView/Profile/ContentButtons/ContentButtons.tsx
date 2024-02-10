e3fe2c6dbc791c8285342d0e00f4d4815d93b156
lumi-sg <shaicloud@gmail.com>
2024-02-10 18:17:33 -0500

    added profile posts/likes sections

--- client/src/Components/Dashboard/MainView/Profile/ContentButtons/ContentButtons.tsx
+++ client/src/Components/Dashboard/MainView/Profile/ContentButtons/ContentButtons.tsx
@@ -1,14 +1,27 @@
 import { Group, Button, Divider } from "@mantine/core";
+import { ProfileViewType } from "../Profile";
 
-const ContentButtons = () => {
+type ContentButtonPros = {
+	handleProfileViewClick: (view: ProfileViewType) => void;
+};
+
+const ContentButtons = ({ handleProfileViewClick }: ContentButtonPros) => {
 	return (
-		<Group gap={0} mt={10} mb={10} w={"100%"} justify="center" align="center">
+		<Group
+			gap={0}
+			mt={10}
+			mb={10}
+			w={"100%"}
+			justify="center"
+			align="center"
+		>
 			<Button
 				w={"49%"}
 				radius="md"
 				size="md"
 				variant="subtle"
 				color="violet"
+				onClick={() => handleProfileViewClick("posts")}
 			>
 				Posts
 			</Button>
@@ -19,6 +32,7 @@ const ContentButtons = () => {
 				size="md"
 				variant="subtle"
 				color="violet"
+				onClick={() => handleProfileViewClick("likes")}
 			>
 				Likes
 			</Button>
--- client/src/Components/Dashboard/MainView/Profile/Profile.tsx
+++ client/src/Components/Dashboard/MainView/Profile/Profile.tsx
@@ -1,14 +1,35 @@
 import { UserCardImage } from "./UserInfo/UserInfo";
 import { Flex, Divider } from "@mantine/core";
 import ContentButtons from "./ContentButtons/ContentButtons";
+import { useState } from "react";
+import Posts from "./Posts/Posts";
+import Likes from "./Likes/Likes";
+
+export type ProfileViewType = "posts" | "likes";
 
 const Profile = () => {
+	const [profileView, setProfileView] = useState<ProfileViewType>("posts");
+
+	const handleProfileViewClick = (view: ProfileViewType) => {
+		setProfileView(view);
+	};
+
+	const renderProfileView = () => {
+		switch (profileView) {
+			case "posts":
+				return <Posts />;
+			case "likes":
+				return <Likes />;
+		}
+	};
+
 	return (
 		<div>
 			<Flex h={"100%"} w={"100%"} direction="column">
 				<UserCardImage />
-				<ContentButtons />
+				<ContentButtons handleProfileViewClick={handleProfileViewClick} />
 				<Divider />
+				{renderProfileView()}
 			</Flex>
 		</div>
 	);
