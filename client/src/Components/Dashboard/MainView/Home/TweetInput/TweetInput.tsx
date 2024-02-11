32ee49b9c1bdc06c283a95fbcb68772eab0987aa
lumi-sg <shaicloud@gmail.com>
2024-02-11 01:41:35 -0500

    initial complete layout for tweet composing

--- client/src/Components/Dashboard/MainView/Home/TweetInput/TweetInput.tsx
+++ client/src/Components/Dashboard/MainView/Home/TweetInput/TweetInput.tsx
@@ -1,13 +1,89 @@
 import { useUserStore } from "../../../../../Stores/userStore";
-import { Avatar, Group, Paper } from "@mantine/core";
+import {
+	Avatar,
+	Group,
+	Paper,
+	Textarea,
+	RingProgress,
+	Text,
+	Divider,
+	Button,
+} from "@mantine/core";
+import "./test.css";
+import { useEffect, useState } from "react";
 const TweetInput = () => {
 	const { user } = useUserStore();
+	const [tweetInput, setTweetInput] = useState("");
+	const [tweetCharacterLength, setTweetCharacterLength] = useState(0);
+
+	useEffect(() => {
+		setTweetCharacterLength(tweetInput.length);
+	}, [tweetInput]);
 
 	return (
 		<div>
-			<Paper withBorder radius="md" p={10}>
-				<Group mb={10}>
-					<Avatar src={user?.photoURL} size={40} radius="xl" />
+			<Paper withBorder radius="md" p={10} w={"100%"} h={"100%"}>
+				<Group h={"100%"} align="center">
+					<Avatar
+						src={user?.photoURL}
+						size={40}
+						radius="xl"
+						m={10}
+						ml={21}
+					/>
+					<Textarea
+						variant="unstyled"
+						autosize
+						h={"100%"}
+						w={"80%"}
+						maxRows={6}
+						placeholder="What's happening"
+						size="lg"
+						style={{
+							backgroundColor: "#242424",
+						}}
+						value={tweetInput}
+						onChange={(e) => setTweetInput(e.target.value)}
+						c={"white"}
+					></Textarea>
+				</Group>
+				<Group justify="flex-end">
+					<RingProgress
+						size={35}
+						thickness={4}
+						label={
+							<Text
+								size="xs"
+								ta={"center"}
+								c={
+									tweetCharacterLength > 280
+										? "rgba(255, 10, 10, 0.7)"
+										: "white"
+								}
+							>
+								{280 - tweetCharacterLength <= 20
+									? 280 - tweetCharacterLength
+									: null}
+							</Text>
+						}
+						sections={[
+							{
+								value: tweetCharacterLength / 2.8, //This is because the spinner's max value is 100
+								color:
+									tweetCharacterLength > 280
+										? "#242424"
+										: "#8769dd",
+							},
+						]}
+					/>
+					<Divider orientation="vertical" ml={5} mr={5} size="xs" />
+					<Button
+						color="violet"
+						variant="outline"
+						disabled={!tweetCharacterLength}
+					>
+						Post
+					</Button>
 				</Group>
 			</Paper>
 		</div>
