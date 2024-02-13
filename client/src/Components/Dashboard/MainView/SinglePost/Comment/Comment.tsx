13eec9c05b6be7a8553342e75ddd4de8a4c0a6bc
lumi-sg <shaicloud@gmail.com>
2024-02-13 01:02:05 -0500

    more comment styling

--- client/src/Components/Dashboard/MainView/SinglePost/Comment/Comment.tsx
+++ client/src/Components/Dashboard/MainView/SinglePost/Comment/Comment.tsx
@@ -4,17 +4,27 @@ import {
 	Group,
 	TypographyStylesProvider,
 	Paper,
+	Button,
 } from "@mantine/core";
 import classes from "./Commentcomponent.module.css";
 import styles from "./Commentcomponent.module.css";
 import { useUserStore } from "../../../../../Stores/userStore";
 import { convertEmailToUsername } from "../../../../../Helpers/convertEmailToUsername";
-import { MessageCircle2, Heart, Bookmark } from "tabler-icons-react";
+import { MessageCircle2, Heart, Bookmark, Dots } from "tabler-icons-react";
 import { useComponentStore } from "../../../../../Stores/componentStore";
 
 const Comment = () => {
 	const { user } = useUserStore();
 	const { setSelectedComponent } = useComponentStore();
+
+	const handleDotsClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		console.log("dots clicked");
+	};
+	const handleActionClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation();
+		console.log("action clicked");
+	};
 	return (
 		<Paper
 			radius="md"
@@ -22,31 +32,42 @@ const Comment = () => {
 			onClick={() => setSelectedComponent("SinglePost")}
 			style={{ borderBottom: "3px solid #8d7ac8" }}
 		>
-			<Group>
-				<Avatar
-					src={user?.photoURL}
-					alt={user?.displayName as string}
-					radius="xl"
-				/>
-				<div>
-					<Group gap={5}>
-						<Text
-							fz="md"
-							fw={700}
-							c={"white"}
-							style={{ cursor: "pointer" }}
-						>
-							{user?.displayName as string}
-						</Text>
-						<Text fz="xs" c="#b097fcce">
-							{convertEmailToUsername(user?.email as string)}
-						</Text>
-					</Group>
+			<Group justify="space-between">
+				<Group>
+					<Avatar
+						src={user?.photoURL}
+						alt={user?.displayName as string}
+						radius="xl"
+					/>
+					<div>
+						<Group gap={5}>
+							<Text
+								fz="md"
+								fw={700}
+								c={"white"}
+								style={{ cursor: "pointer" }}
+							>
+								{user?.displayName as string}
+							</Text>
+							<Text fz="xs" c="#b097fcce">
+								{convertEmailToUsername(user?.email as string)}
+							</Text>
+						</Group>
 
-					<Text fz="xs" c="dimmed">
-						10 minutes ago
-					</Text>
-				</div>
+						<Text fz="xs" c="dimmed">
+							10 minutes ago
+						</Text>
+					</div>
+				</Group>
+
+				<Button
+					color="violet"
+					variant="subtle"
+					size="xs"
+					onClick={(e) => handleDotsClick(e)}
+				>
+					<Dots />
+				</Button>
 			</Group>
 			<TypographyStylesProvider className={classes.body}>
 				<Text c="white">
@@ -61,7 +82,11 @@ const Comment = () => {
 					mt={10}
 					align="center"
 				>
-					<Group gap={2} className={styles.messageicon}>
+					<Group
+						gap={2}
+						className={styles.messageicon}
+						onClick={(e) => handleActionClick(e)}
+					>
 						<MessageCircle2
 							size={22}
 							className={styles.messageActualIcon}
@@ -70,13 +95,21 @@ const Comment = () => {
 							53
 						</Text>
 					</Group>
-					<Group gap={2} className={styles.hearticon}>
+					<Group
+						gap={2}
+						className={styles.hearticon}
+						onClick={(e) => handleActionClick(e)}
+					>
 						<Heart size={22} className={styles.heartActualIcon} />
 						<Text c={"white"} size="sm" fw={600}>
 							100
 						</Text>
 					</Group>
-					<Group gap={0} className={styles.bookmarkicon}>
+					<Group
+						gap={0}
+						className={styles.bookmarkicon}
+						onClick={(e) => handleActionClick(e)}
+					>
 						<Bookmark
 							size={22}
 							className={styles.bookmarkActualIcon}
--- client/src/Components/ModularComponents/TweetComponent/TweetComponent.tsx
+++ client/src/Components/ModularComponents/TweetComponent/TweetComponent.tsx
@@ -4,17 +4,28 @@ import {
 	Group,
 	TypographyStylesProvider,
 	Paper,
+	Button,
 } from "@mantine/core";
 import classes from "./TweetComponent.module.css";
 import styles from "./TweetComponent.module.css";
 import { useUserStore } from "../../../Stores/userStore";
 import { convertEmailToUsername } from "../../../Helpers/convertEmailToUsername";
-import { MessageCircle2, Heart, Bookmark } from "tabler-icons-react";
+import { MessageCircle2, Heart, Bookmark, Dots } from "tabler-icons-react";
 import { useComponentStore } from "../../../Stores/componentStore";
 
 export function TweetComponent() {
 	const { user } = useUserStore();
 	const { setSelectedComponent } = useComponentStore();
+
+	const handleDotsClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		console.log("dots clicked");
+	};
+
+	const handleActionClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation();
+		console.log("action clicked");
+	};
 
 	return (
 		<Paper
@@ -24,31 +35,42 @@ export function TweetComponent() {
 			onClick={() => setSelectedComponent("SinglePost")}
 			style={{ borderLeft: "5px solid #8d7ac8" }}
 		>
-			<Group>
-				<Avatar
-					src={user?.photoURL}
-					alt={user?.displayName as string}
-					radius="xl"
-				/>
-				<div>
-					<Group gap={5}>
-						<Text
-							fz="md"
-							fw={700}
-							c={"white"}
-							style={{ cursor: "pointer" }}
-						>
-							{user?.displayName as string}
-						</Text>
-						<Text fz="xs" c="#b097fcce">
-							{convertEmailToUsername(user?.email as string)}
-						</Text>
-					</Group>
+			<Group justify="space-between">
+				<Group>
+					<Avatar
+						src={user?.photoURL}
+						alt={user?.displayName as string}
+						radius="xl"
+					/>
+					<div>
+						<Group gap={5}>
+							<Text
+								fz="md"
+								fw={700}
+								c={"white"}
+								style={{ cursor: "pointer" }}
+							>
+								{user?.displayName as string}
+							</Text>
+							<Text fz="xs" c="#b097fcce">
+								{convertEmailToUsername(user?.email as string)}
+							</Text>
+						</Group>
 
-					<Text fz="xs" c="dimmed">
-						10 minutes ago
-					</Text>
-				</div>
+						<Text fz="xs" c="dimmed">
+							10 minutes ago
+						</Text>
+					</div>
+				</Group>
+
+				<Button
+					color="violet"
+					variant="subtle"
+					size="xs"
+					onClick={(e) => handleDotsClick(e)}
+				>
+					<Dots />
+				</Button>
 			</Group>
 			<TypographyStylesProvider className={classes.body}>
 				<Text c="white">
@@ -63,7 +85,11 @@ export function TweetComponent() {
 					mt={10}
 					align="center"
 				>
-					<Group gap={2} className={styles.messageicon}>
+					<Group
+						gap={2}
+						className={styles.messageicon}
+						onClick={(e) => handleActionClick(e)}
+					>
 						<MessageCircle2
 							size={22}
 							className={styles.messageActualIcon}
@@ -72,13 +98,21 @@ export function TweetComponent() {
 							53
 						</Text>
 					</Group>
-					<Group gap={2} className={styles.hearticon}>
+					<Group
+						gap={2}
+						className={styles.hearticon}
+						onClick={(e) => handleActionClick(e)}
+					>
 						<Heart size={22} className={styles.heartActualIcon} />
 						<Text c={"white"} size="sm" fw={600}>
 							100
 						</Text>
 					</Group>
-					<Group gap={0} className={styles.bookmarkicon}>
+					<Group
+						gap={0}
+						className={styles.bookmarkicon}
+						onClick={(e) => handleActionClick(e)}
+					>
 						<Bookmark
 							size={22}
 							className={styles.bookmarkActualIcon}
