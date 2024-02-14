02b11d6d0a5ae505d850c821e0b9a4a13adaf129
lumi-sg <shaicloud@gmail.com>
2024-02-13 23:50:06 -0500

    made it so going to home page resets state of author

--- client/src/Components/Dashboard/MainView/Home/Home.tsx
+++ client/src/Components/Dashboard/MainView/Home/Home.tsx
@@ -1,8 +1,15 @@
 import { TweetComponent } from "../../../Features/TweetComponent/TweetComponent";
 import TweetInput from "../../../Features/TweetInput/TweetInput";
 import { TweetVariant } from "../../../../constants/TweetVariant";
+import { useParentTweetStoreAuthor } from "../../../../Stores/parentTweetStoreAuthor";
+import { useEffect } from "react";
 
 const Home = () => {
+	const { setParentTweetAuthor } = useParentTweetStoreAuthor();
+
+	useEffect(() => {
+		setParentTweetAuthor(null);
+	}, []);
 	return (
 		<>
 			<TweetInput placeholderMessage="What's happening" isReply={false} />
