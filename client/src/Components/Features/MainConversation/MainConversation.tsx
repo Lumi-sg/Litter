e59e0bece4aa85ab750bef38ad729fe755cef333
lumi-sg <shaicloud@gmail.com>
2024-02-15 21:28:35 -0500

    intial conversation module done

--- client/src/Components/Features/MainConversation/MainConversation.tsx
+++ client/src/Components/Features/MainConversation/MainConversation.tsx
@@ -1,9 +1,46 @@
-import { ScrollArea } from "@mantine/core";
+import { Divider, ScrollArea, Stack } from "@mantine/core";
+import MessageCard from "../MessageCard/MessageCard";
+import { useRef, useEffect } from "react";
 
 const MainConversation = () => {
+	const scrollHere = useRef<HTMLDivElement>(null);
+
+	const handleScroll = () => {
+		if (scrollHere.current) {
+			scrollHere.current.scrollIntoView({ behavior: "smooth" });
+		}
+	};
+
+	useEffect(() => {
+		handleScroll();
+	}, []);
 	return (
-		<ScrollArea h={"100%"}>
-			<div>hi</div>
+		<ScrollArea h={"100%"} offsetScrollbars scrollbarSize={2}>
+			<Stack mr={5}>
+				<Stack align="flex-end">
+					<MessageCard userMessage={false} />
+				</Stack>
+				<Stack align="flex-start">
+					<MessageCard userMessage={true} />
+				</Stack>{" "}
+				<Stack align="flex-end">
+					<MessageCard userMessage={false} />
+				</Stack>{" "}
+				<Stack align="flex-start">
+					<MessageCard userMessage={true} />
+				</Stack>{" "}
+				<Stack align="flex-end">
+					<MessageCard userMessage={false} />
+				</Stack>{" "}
+				<Stack align="flex-start">
+					<MessageCard userMessage={true} />
+				</Stack>{" "}
+				<Stack align="flex-end">
+					<MessageCard userMessage={false} />
+				</Stack>
+			</Stack>
+			<div ref={scrollHere}></div>
+			<Divider mt={10}/>
 		</ScrollArea>
 	);
 };
--- client/src/Components/Features/MessageCard/MessageCard.tsx
+++ client/src/Components/Features/MessageCard/MessageCard.tsx
@@ -1,8 +1,27 @@
+import { Text,Box } from "@mantine/core";
 
-const MessageCard = () => {
-  return (
-    <div>MessageCard</div>
-  )
-}
+type MessageCardProps = {
+	userMessage: boolean;
+};
+const MessageCard = ({ userMessage }: MessageCardProps) => {
+	return (
+		<>
+			<Box
+				bg={userMessage ? "#2e2e2e" : "#2C2A32"}
+				p={10}
+				style={{ borderRadius: 10, border: "1px solid #8d7ac8" }}
+				w={"35%"}
+			>
+				<Text size="sm" c={"white"}>
+					This Pokémon likes to lick its palms that are sweetened by
+					being soaked in honey.
+				</Text>
+			</Box>
+			<Text size="xs" c="dimmed" mt={-15}>
+				10 minutes ago
+			</Text>
+		</>
+	);
+};
 
-export default MessageCard
+export default MessageCard;
