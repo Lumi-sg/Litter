3093e929a450dd84f98ef0ed15d5bb8b47568b2a
lumi-sg <shaicloud@gmail.com>
2024-03-01 17:17:20 -0500

    added conversation model

---
+++ server/src/models/Conversation.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+import { UserType } from "./User";
+
+export type MessageType = Document & {
+	_id?: Types.ObjectId;
+	content: string;
+	sender: UserType;
+	receiver: UserType;
+	timestamp: Date;
+};
+
+export type ConversationType = Document & {
+	_id?: Types.ObjectId;
+	participants: UserType[];
+	messages: MessageType[];
+};
+
+const conversationSchema = new Schema<ConversationType>({
+	participants: [{ type: Schema.Types.ObjectId, ref: "User" }],
+	messages: [{ type: Object, required: true }],
+});
+
+export const ConversationModel = mongoose.model<ConversationType>(
+	"Conversation",
+	conversationSchema
+);
+
+export default ConversationModel;
