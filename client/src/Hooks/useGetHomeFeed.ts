695d840baf163880a3c08f3496111a2cb968a332
lumi-sg <shaicloud@gmail.com>
2024-03-06 18:17:17 -0500

    added get home feed hook

---
+++ client/src/Hooks/useGetHomeFeed.ts
@@ -0,0 +1,26 @@
+import Cookies from "js-cookie";
+import { baseURL } from "../constants/baseURL";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import TweetType from "../Types/Tweet";
+import { useUserStore } from "../Stores/userStore";
+
+export const useGetHomeFeed = () => {
+    const {user} = useUserStore();
+	const firebaseToken = Cookies.get("firebaseToken");
+
+	return useQuery({
+		queryKey: ["homefeed", user?.uid],
+		queryFn: async () => {
+			const { data } = await axios.get(`${baseURL}/user/homefeed`, {
+				headers: {
+					Authorization: `Bearer ${firebaseToken}`,
+				},
+			});
+            console.table(data.tweets)
+			return data.tweets as TweetType[];
+		},
+	});
+};
+
+export default useGetHomeFeed;
