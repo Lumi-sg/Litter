0c487d33b6f56d6ab8b0d408d05957baaf6e57bd
lumi-sg <shaicloud@gmail.com>
2024-03-07 22:39:43 -0500

    added notification model

---
+++ server/src/models/Notification.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+import { TweetType } from "./Tweet";
+import { UserType } from "./User";
+import { ConversationType } from "./Conversation";
+
+export type NotificationType = Document & {
+	_id?: Types.ObjectId;
+	recipient: UserType;
+	sender: UserType;
+	type: string;
+	tweet?: TweetType;
+	conversation?: ConversationType;
+	read: boolean;
+	createdAt: Date;
+};
+
+const notificationSchema = new Schema<NotificationType>({
+	recipient: { type: Schema.Types.ObjectId, ref: "User" },
+	sender: { type: Schema.Types.ObjectId, ref: "User" },
+	type: { type: String, required: true },
+	tweet: { type: Schema.Types.ObjectId, ref: "Tweet" },
+	conversation: { type: Schema.Types.ObjectId, ref: "Conversation" },
+	read: { type: Boolean, default: false },
+	createdAt: { type: Date, default: Date.now },
+});
+
+export const NotificationModel = mongoose.model<NotificationType>(
+	"Notification",
+	notificationSchema
+);
+
+export default NotificationModel;
