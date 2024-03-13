dd2484bf2296e62016dbd4ccbc51929591a82fa9
lumi-sg <shaicloud@gmail.com>
2024-03-13 01:32:25 -0400

    basic conversation layout stuff TODO: add routing to conversation

--- client/src/Components/Dashboard/MainView/Messages/RightMessageContainer/RightMessageContainer.tsx
+++ client/src/Components/Dashboard/MainView/Messages/RightMessageContainer/RightMessageContainer.tsx
@@ -1,17 +1,32 @@
 import ConversationHeader from "../../../../Features/ConversationHeader/ConversationHeader";
 import MainConversation from "../../../../Features/MainConversation/MainConversation";
 import ConversationInput from "../../../../Features/ConversationInput/ConversationInput";
-import { Stack} from "@mantine/core";
+import { Center, Stack } from "@mantine/core";
+import { useParams, } from "react-router-dom";
+import { useGetConversation } from "../../../../../Hooks/useGetConversation";
+import LoadingTweet from "../../../../Features/LoadingTweet/LoadingTweet";
 
 const RightMessageContainer = () => {
+	const { conversationID } = useParams();
+	const { data: conversation, isLoading } = useGetConversation(
+		conversationID as string
+	);
 	return (
-		<Stack
-			h={"calc(100vh - 5.7rem)"}
-			flex={1}
-		>
-			<ConversationHeader />
-			<MainConversation />
-			<ConversationInput />
+		<Stack h={"calc(100vh - 5.7rem)"} flex={1}>
+			{" "}
+			{!conversationID ? (
+				<Center h={"100%"} w={"100%"}>
+					Start a new conversation!
+				</Center>
+			) : isLoading ? (
+				<LoadingTweet />
+			) : (
+				<>
+					<ConversationHeader />
+					<MainConversation />
+					<ConversationInput />
+				</>
+			)}
 		</Stack>
 	);
 };
