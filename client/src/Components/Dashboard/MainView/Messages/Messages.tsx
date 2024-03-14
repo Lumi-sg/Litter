329341d167f7aeb74ae8c027475a983b37d23338
lumi-sg <shaicloud@gmail.com>
2024-03-14 01:04:23 -0400

    added placeholder empty container for when no conversation is picked

--- client/src/Components/Dashboard/MainView/Messages/Messages.tsx
+++ client/src/Components/Dashboard/MainView/Messages/Messages.tsx
@@ -1,15 +1,11 @@
 import { useEffect } from "react";
 import LeftMessageContainer from "./LeftMessageContainer/LeftMessageContainer";
 import RightMessageContainer from "./RightMessageContainer/RightMessageContainer";
-import { Divider, Group } from "@mantine/core";
+import { Center, Divider, Flex, Group, Stack } from "@mantine/core";
 import { useComponentStore } from "../../../../Stores/componentStore";
 import { Route, Routes } from "react-router-dom";
 
-
-
 const Messages = () => {
-
-
 	const { setSelectedComponent } = useComponentStore();
 
 	useEffect(() => {
@@ -21,6 +17,29 @@ const Messages = () => {
 			<LeftMessageContainer />
 			<Divider orientation="vertical" />
 			<Routes>
+				<Route
+					path="/"
+					element={
+						<Flex
+							h={"calc(100vh - 5.7rem)"}
+							flex={1}
+							justify={"center"}
+							align={"center"}
+						>
+							<Center>
+								<div
+									style={{
+										fontSize: "1.2rem",
+										color: "white",
+									}}
+								>
+									Start a new conversation!
+								</div>
+							</Center>
+						</Flex>
+					}
+				/>
+
 				<Route
 					path="/:conversationID"
 					element={<RightMessageContainer />}
