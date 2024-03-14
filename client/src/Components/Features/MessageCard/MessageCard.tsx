ba2a4620574e13cf65324aebc1320fc5a89eea71
lumi-sg <shaicloud@gmail.com>
2024-03-14 02:12:58 -0400

    conversation styling

--- client/src/Components/Features/MessageCard/MessageCard.tsx
+++ client/src/Components/Features/MessageCard/MessageCard.tsx
@@ -1,4 +1,4 @@
-import { Text, Box } from "@mantine/core";
+import { Text, Box, Center } from "@mantine/core";
 import { MessageType } from "../../../Types/Message";
 import formatTimeStamp from "../../../Helpers/formatTimeStamp";
 
@@ -11,20 +11,36 @@ const MessageCard = ({ message, isLoggedInUserMessage }: MessageCardProps) => {
 		<>
 			<Box
 				bg={!isLoggedInUserMessage ? "#2e2e2e" : "#2C2A32"}
-				p={20}
+				p={15}
 				style={{
-					borderRadius: 10,
 					border: !isLoggedInUserMessage
-						? "1px solid #424242"
+						? "1px solid gray"
 						: "1px solid #8d7ac8",
+					borderRadius: "20px",
+					borderBottomLeftRadius: !isLoggedInUserMessage
+						? "5px"
+						: "20px",
+					borderBottomRightRadius: !isLoggedInUserMessage
+						? "20px"
+						: "5px",
+					borderTopLeftRadius: "20px",
+					borderTopRightRadius: "20px",
 				}}
-				w={"40%"}
+				w={"fit-content"}
+				mt={10}
+				maw={"50%"}
 			>
-				<Text size="sm" c={"white"}>
-					{message.content}
-				</Text>
+				<Center>
+					<Text
+						size="md"
+						c={"white"}
+						ta="left"
+					>
+						{message.content}
+					</Text>
+				</Center>
 			</Box>
-			<Text size="xs" c="dimmed" mt={-15}>
+			<Text size="xs" c="dimmed" mt={-15} mb={10}>
 				{formatTimeStamp(message.timestamp.toString())}
 			</Text>
 		</>
