79ec6689f9e5f272c82f78240258f5a82c6f5632
lumi-sg <shaicloud@gmail.com>
2024-05-07 13:32:59 -0400

    fixed some old code

--- client/src/Hooks/Conversation Hooks/useCreateNewMessage.ts
+++ client/src/Hooks/Conversation Hooks/useCreateNewMessage.ts
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 import { baseURL } from "../../constants/baseURL";
 import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
-import { useQueryClient } from "@tanstack/react-query";
+// import { useQueryClient } from "@tanstack/react-query";
 import { displayNotification } from "../../Helpers/displayNotification";
 
 export const useCreateNewMessage = (
@@ -11,7 +11,7 @@ export const useCreateNewMessage = (
 	recipientUsername: string
 ) => {
 	const firebaseToken = Cookies.get("firebaseToken");
-	const queryClient = useQueryClient();
+	// const queryClient = useQueryClient();
 	return useMutation({
 		mutationFn: async () => {
 			const { data } = await axios.post(
