bb21fc5d81b3c639d5ceede45b71f02c8613a00f
lumi-sg <shaicloud@gmail.com>
2024-05-08 16:23:31 -0400

    added optimistic updating for notifications

--- client/src/Hooks/Notification Hooks/useMarkSingleNotificationRead.ts
+++ client/src/Hooks/Notification Hooks/useMarkSingleNotificationRead.ts
@@ -5,6 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useQueryClient } from "@tanstack/react-query";
 import { NotificationType } from "../../Types/Notifications";
 import { useUserStore } from "../../Stores/userStore";
+import { displayNotification } from "../../Helpers/displayNotification";
 
 export const useMarkSingleNotificationRead = () => {
 	const firebaseToken = Cookies.get("firebaseToken");
@@ -13,7 +14,6 @@ export const useMarkSingleNotificationRead = () => {
 
 	return useMutation({
 		mutationFn: async (notificationID: string) => {
-
 			const { data } = await axios.post(
 				`${baseURL}/user/marknotificationread/${notificationID}`,
 				{},
@@ -26,11 +26,56 @@ export const useMarkSingleNotificationRead = () => {
 			return data.notification as NotificationType;
 		},
 
-		onSuccess: () => {
+		onMutate: (notificationID: string) => {
+			queryClient.setQueryData(
+				["notifications", user?.uid as string],
+				(prevNotifications: NotificationType[]) => {
+					const updatedNotifications = prevNotifications.map(
+						(notification) => {
+							if (notification._id === notificationID) {
+								return {
+									...notification,
+									read: true,
+								};
+							}
+							return notification;
+						}
+					);
+					return updatedNotifications;
+				}
+			);
+		},
 
+		onSuccess: () => {
 			queryClient.invalidateQueries({
 				queryKey: ["notifications", user?.uid as string],
 			});
+		},
+		onError: (notificationID: string) => {
+			queryClient.setQueryData(
+				["notifications", user?.uid as string],
+				(prevNotifications: NotificationType[]) => {
+					const updatedNotifications = prevNotifications.map(
+						(notification) => {
+							if (notification._id === notificationID) {
+								return {
+									...notification,
+									read: false,
+								};
+							}
+							return notification;
+						}
+					);
+					return updatedNotifications;
+				}
+			);
+			displayNotification(
+				"Error",
+				"Failed to mark notification as read",
+				"#f87171",
+				"",
+				""
+			);
 		},
 	});
 };
